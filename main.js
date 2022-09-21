75547665a43343e9455faf9c047fc7b2deec3631
Irene Alcaine <irenealcainealvarez@gmail.com>
2022-09-21 23:09:48 +0200

    add api call and .env

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ const currentTempEl = document.getElementById('current-temp')
 const days = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado']
 const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
 
+const API_KEY = import.meta.env.VITE_API_KEY
+
 setInterval(() => {
   const time = new Date()
   const month = time.getMonth()
@@ -21,5 +23,19 @@ setInterval(() => {
   dateEl.innerHTML = days[day] + ', ' + date + ' de ' + months[month]
 
 }, 1000)
+
+getWeatherData()
+function getWeatherData() {
+  navigator.geolocation.getCurrentPosition((succes) => {
+    let { latitude, longitude } = succes.coords
+
+    fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,alerts&appid=${API_KEY}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        showWeatherData(data)
+      })
+  })
+}
 
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'image': "url('https://images.unsplash.com/photo-1525490829609-d166ddb58678?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1769&q=80')",
+      }
+    },
   },
   plugins: [],
 }
