7a9bef074f6c407c153241ee8b79bb1126a743ee
Irene Alcaine <irenealcainealvarez@gmail.com>
2022-09-22 18:30:18 +0200

    add more info

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ function getWeatherData() {
 }
 
 function showWeatherData(data) {
-  let { humidity, wind_speed, weather, temp } = data.current
+  let { humidity, wind_speed, weather, temp, rain } = data.current
   currentWeatherItemsEl.innerHTML =
     `
     <div class="flex items-center justify-around">
@@ -59,6 +59,7 @@ function showWeatherData(data) {
       <div>Viento </div>
       <div>${wind_speed} m/s</div>
     </div>
+
   `
 
   // <div class="flex justify-between">
@@ -81,13 +82,22 @@ function showWeatherData(data) {
       otherDayForecast +=
         `
         <div class="bg-blue-700 rounded-xl py-2 px-4 w-full">
-            <div class="flex justify-between items-center">
-              <div class="day">${showDay}</div>
-              <div class="flex items-center justify-end">
-                <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}.png" alt="weather icon" class="bg-sky-500 rounded-full w-10">
-                <div class="temp">${Math.round(day.temp.min)}&#176; C - ${Math.round(day.temp.max)}&#176; C</div>
+          <details>
+            <summary class="list-none">
+              <div class="flex justify-between items-center">
+                <div class="day">${showDay}</div>
+                <div class="flex items-center justify-end">
+                  <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}.png" alt="weather icon" class="bg-sky-500 rounded-full w-10">
+                  <div class="temp">${Math.round(day.temp.min)}&#176; C - ${Math.round(day.temp.max)}&#176; C</div>
+                </div>
               </div>
+            </summary>
+            <div class="border-2 rounded-xl px-4 py-2 mt-2">
+              <div>Humedad: ${day.humidity}%</div>
+              <div>Viento: ${day.wind_speed} m/s</div>
+              <div>Lluvia: ${day.rain} mm</div>
             </div>
+          </details>
         </div>
         `
     }
